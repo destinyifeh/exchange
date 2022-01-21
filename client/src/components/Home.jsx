b60cf3f7b55ad11756d33ef98f175e84f4776712
destinyifeh <destinyifeh@gmail.com>
2022-01-20 12:53:46 -1200

    updated

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -11,6 +11,10 @@ function Home(){
         
             const [data, setData] = useState('');
             const [remaining, setRemaining] = useState(false);
+             const [remaining2, setRemaining2] = useState(false);
+            const [remaining3, setRemaining3] = useState(false);
+            const [remaining4, setRemaining4] = useState(false);
+
             const [loader, setLoader] = useState(true);
             
 
@@ -31,9 +35,46 @@ function Home(){
               console.log(e)
               setRemaining(false);
           }
+
+
+           let handleRemaining2 = () =>{
+             
+              setRemaining2(true);
+              
+            }
+
+              
+            let handleLess2 = (e) =>{
+              console.log(e)
+              setRemaining2(false);
+          }
             
                
               
+            let handleRemaining3 = () =>{
+             
+              setRemaining3(true);
+              
+            }
+
+              
+            let handleLess3 = (e) =>{
+              console.log(e)
+              setRemaining3(false);
+          }
+
+          
+            let handleRemaining4 = () =>{
+             
+              setRemaining4(true);
+              
+            }
+
+              
+            let handleLess4 = (e) =>{
+              console.log(e)
+              setRemaining4(false);
+          }
           
 
           useEffect(()=>{
@@ -53,6 +94,21 @@ function Home(){
 
             const bitcoinData = Array.from(data);
              console.log(bitcoinData)
+
+             let description = 'El Salvador Prepares to Launchr Prepares to Launch $1 Billion Bitcoin Bond to Build a Bitcoin City…(and buy more Bitcoin)';
+             let title = 'El Salvador Prepares to Launch $1 Billion Bitcoin Bond to Build a Bitcoin City…(and buy more Bitcoin)';
+  
+              let des2 = 'Bitcoin vs Altcoins: Bitcoin W Altcoins: Bitcoin Will Outperform Altcoins in Q4 ';
+              let title2 = 'Bitcoin vs Altcoins: Bitcoin W Altcoins: Bitcoin Will Outperform Altcoins in Q4 ';
+
+               let des3 = ' Cathedra Bitcoin Inc. (TSX-V: CBIT) (the “Company” or “Cathedra”), a Bitcoin company that develops and operates world-class bitcoin mining infrastructure, is pleased to announce that it will donate US$50,000 of bitcoin to Summer of Bitcoin, a global summer internship program focused on introducing university students to Bitcoin and open-source development. The donation from  ';
+              let title3 = ' Cathedra Bitcoin Announces Donation to Summer of Bitcoin';
+
+               let des4 = 'You might have already heard thave already heard the buzz about the Bitcoin Code app, which has become a popular trading platform recently. This software has garnered a lot of attention for its powerful and accurate forecasts on Bitcoin price swings (99 percent accuracy). The Bitcoin Code software is completely automated, and even new traders can use it';
+              let title4 = 'Build Significant Wealth by Joining the Bitcoin Boom with Bitcoin Code ';
+  
+  
+  
     return(
      
        <>
@@ -140,9 +196,58 @@ function Home(){
       
        <div className="container my-5 blog">
          <h4 className="blog-post text-center my-4"> Bitcoin News</h4>
-         {bitcoinData.length>0? ' ' : <p className="text-center">No bitcoin news available yet</p>}
-         {loader? <p className="text-center">Loading bitcoin news...</p>: ''}
+          {loader? <p className="text-center">Loading bitcoin news...</p>: ''}
 
+         {bitcoinData.length === 0 ?   
+
+
+           <div className="card-group">
+        
+           <div className="card mx-1" >
+             <img className="img-card-top" src={btcImage} alt=""/>
+             <div className="card-body">
+               <h5 className="card-title">{title}</h5>
+               <p className='card-text'>{description.substring(0, 30)}{remaining?'' :<span>...</span>}{remaining?<span className="remaining">{description.substring(10, 1000)}</span>: ' '}</p>
+                <Link className="read" to="#" onClick={handleRemaining}>{remaining?'' :'Readmore'}</Link>
+                 <Link className="read" to="#" onClick={handleLess}>{remaining?'Readless' :''}</Link>
+
+             </div>
+           </div>
+
+           <div className="card mx-1" >
+             <img className="img-card-top" src={btcImage} alt=""/>
+             <div className="card-body">
+               <h5 className="card-title">{title2}</h5>
+               <p className='card-text'>{des2.substring(0, 30)}{remaining2?'' :<span>...</span>}{remaining2?<span className="remaining">{description.substring(10, 1000)}</span>: ' '}</p>
+                <Link className="read" to="#" onClick={handleRemaining2}>{remaining2?'' :'Readmore'}</Link>
+                 <Link className="read" to="#" onClick={handleLess2}>{remaining2?'Readless' :''}</Link>
+
+             </div>
+           </div>
+
+           <div className="card mx-1" >
+             <img className="img-card-top" src={btcImage} alt=""/>
+             <div className="card-body">
+               <h5 className="card-title">{title3}</h5>
+               <p className='card-text'>{des3.substring(0, 30)}{remaining3?'' :<span>...</span>}{remaining3?<span className="remaining">{description.substring(10, 1000)}</span>: ' '}</p>
+                <Link className="read" to="#" onClick={handleRemaining3}>{remaining3?'' :'Readmore'}</Link>
+                 <Link className="read" to="#" onClick={handleLess3}>{remaining3?'Readless' :''}</Link>
+
+             </div>
+           </div>
+
+           <div className="card mx-1 " >
+             <img className="img-card-top" src={btcImage} alt=""/>
+             <div className="card-body">
+               <h5 className="card-title">{title4}</h5>
+               <p className='card-text'>{des4.substring(0, 30)}{remaining4?'' :<span>...</span>}{remaining4?<span className="remaining">{description.substring(10, 1000)}</span>: ' '}</p>
+                <Link className="read" to="#" onClick={handleRemaining4}>{remaining4?'' :'Readmore'}</Link>
+                 <Link className="read" to="#" onClick={handleLess4}>{remaining4?'Readless' :''}</Link>
+
+             </div>
+           </div>
+          </div>
+           :
          <div className="card-group">
          {bitcoinData.slice(0, 4).map((bitcoin, index)=>{
            return(
@@ -162,6 +267,7 @@ function Home(){
 
            
          </div>
+         }
        </div>
         
          </div>
